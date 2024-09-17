12c1e0535d117f95a9f30dcf080b31bfe8db9a09
tasuboyz <95526476+tasuboyz@users.noreply.github.com>
2024-09-17 12:27:43 +0200

    Create LogoffButton.tsx

---
+++ src/components/LogoffButton.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const LogoffButton = () => {
+    const handleLogoff = () => {
+      const logoff = {
+        logoff: "True"
+      }
+        window.Telegram.WebApp.sendData(JSON.stringify(logoff));
+    };
+
+    return (
+        <button onClick={handleLogoff} style={{ padding: '10px', backgroundColor: '#f44336', color: 'white', border: 'none', borderRadius: '5px', cursor: 'pointer' }}>
+            Logoff
+        </button>
+    );
+};
+
+export default LogoffButton;

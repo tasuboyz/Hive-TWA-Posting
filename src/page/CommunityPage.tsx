d5ea0708be1b8010796fc5a79c37a2e7fb7ff43f
tasuboyz <tasuhiro.davide@gmail.com>
2024-10-02 18:15:13 +0200

    Update CommunityPage Interface

--- src/page/CommunityPage.tsx
+++ src/page/CommunityPage.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, { useState, useEffect, useCallback, useRef } from 'react';
 import './CommunityPage.css';
 import '../App.css'
 import { Telegram } from "@twa-dev/types";
-import { Box, List, ListItem, ListItemText, Container, TextField } from '@mui/material';
+import { Box, List, ListItem, ListItemText, TextField, CircularProgress } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
 
 declare global {
@@ -16,67 +16,71 @@ interface Community {
   name: string;
 }
 
-function PostingPage() {
-  const [communityNames, setCommunityNames] = React.useState<Array<string>>([]);
-  const [community, setCommunity] = React.useState<string>('');
+function CommunityPage() {
+  const [communityNames, setCommunityNames] = useState<Array<string>>([]);
+  const [community, setCommunity] = useState<string>('');
+  const [loading, setLoading] = useState<boolean>(false);
+  const [hasMore, setHasMore] = useState<boolean>(true);
+  const [page, setPage] = useState<number>(1);
   const navigate = useNavigate();
+  const observer = useRef<IntersectionObserver | null>(null);
 
-  const SearchCommunity = React.useCallback(async (): Promise<void> => {
+  const lastCommunityElementRef = useCallback((node: HTMLLIElement) => {
+  if (loading) return;
+  if (observer.current) observer.current.disconnect();
+  observer.current = new IntersectionObserver(entries => {
+    if (entries[0].isIntersecting && hasMore) {
+      setPage(prevPage => prevPage + 1);
+    }
+  });
+  if (node) observer.current.observe(node);
+}, [loading, hasMore]);
+
+  const SearchCommunity = useCallback(async (): Promise<void> => {
+    setLoading(true);
     const headers = {
         "accept": "application/json",
         "Content-Type": "application/json"
     };
 
     try {
-        const response = await fetch(`https://imridd.eu.pythonanywhere.com/api/hive/communities?title=${community}`, {
+        const response = await fetch(`https://imridd.eu.pythonanywhere.com/api/hive/communities?title=${community}&page=${page}&limit=20`, {
             method: 'GET',
             headers: headers,
         });
 
         if (!response.ok) {
-            throw new Error('Errore durante l\'invio del messaggio');
+            throw new Error('Error fetching communities');
         }
         const data = await response.json(); 
-        const Communities = data.map((item: Community) => {
-          return `${item.name},${item.title}`;
-        });
-        setCommunityNames(Communities);
+        const newCommunities = data.map((item: Community) => `${item.name},${item.title}`);
+        setCommunityNames(prev => [...prev, ...newCommunities]);
+        setHasMore(newCommunities.length > 0);
+        setLoading(false);
     } catch (error) {
         window.Telegram.WebApp.showPopup({
-            title: "Errore",
-            message: "Si è verificato un errore durante l'invio del messaggio.",
+            title: "Error",
+            message: "An error occurred while fetching communities.",
             buttons: [{ type: 'ok' }]
         });
-        console.error('Errore durante l\'invio del messaggio:', error);
+        console.error('Error fetching communities:', error);
+        setLoading(false);
     }
-  }, [community]);
+  }, [community, page]);
 
-  React.useEffect(() => {
+  useEffect(() => {
     window.Telegram.WebApp.BackButton.show();
-
     window.Telegram.WebApp.BackButton.onClick(() => {
       window.Telegram.WebApp.BackButton.hide();
       navigate('/Hive-TWA-Posting/post');
     });
 
     SearchCommunity();
-    scrollList();
-  }, [navigate, SearchCommunity]);
-
-  const scrollList = () => {
-    const listElement = document.getElementById('list');
-    if (listElement) {
-      listElement.scrollBy(0, 100); // Scorre di 100px
-    }
-  };
+  }, [navigate, SearchCommunity, page]);
 
   const handleCommunityChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setCommunity(event.target.value);
   };
-
-  // const handleCommunitySelect = (selectedName: string) => {
-  //   setCommunity(selectedName);
-  // };
 
   const handleCommunitySelect = (selectedName: string, selectedId: string) => {
     console.log(`Selected community: ${selectedName}, ID: ${selectedId}`);
@@ -89,10 +93,9 @@ function PostingPage() {
 
   return (
     <div className="container-community">
-    <Container>
-      <Box className="container-community" sx={{ padding: 2 }}>
+      <div className="search-container">
         <TextField
-          id = 'community-input' 
+          id='community-input' 
           label="Search Community"
           value={community}
           onChange={handleCommunityChange}
@@ -100,25 +103,33 @@ function PostingPage() {
           variant="outlined"
           className="community-search"
         />
-        <Box id="list" sx={{ height: '400px', overflowY: 'scroll', marginTop: 2, width: '100%'}}>
-            <List>
-              <ListItem onClick={() => handleCommunitySelect("No community", "None")}>
-                <ListItemText primary="No community" />
+      </div>
+      <div className="list-container">
+        <List id="list">
+          <ListItem onClick={() => handleCommunitySelect("No community", "None")}>
+            <ListItemText primary="No community" />
+          </ListItem>
+          {communityNames.map((item, index) => {
+          const [id, name] = item.split(',');
+          if (communityNames.length === index + 1) {
+            return (
+              <ListItem ref={lastCommunityElementRef} key={index} onClick={() => handleCommunitySelect(name, id)}>
+                <ListItemText primary={name} />
               </ListItem>
-              {communityNames.map((item, index) => {
-                const [id, name] = item.split(',');
-                return (
-                  <ListItem key={index} onClick={() => handleCommunitySelect(name, id)}>
-                    <ListItemText primary={name} />
-                  </ListItem>
-                );
-              })}           
-          </List>
-        </Box>
-      </Box>
-    </Container>
+            );
+          } else {
+            return (
+              <ListItem key={index} onClick={() => handleCommunitySelect(name, id)}>
+                <ListItemText primary={name} />
+              </ListItem>
+            );
+          }
+        })}       
+        </List>
+        {loading && <Box display="flex" justifyContent="center" my={2}><CircularProgress /></Box>}
+      </div>
     </div>
   );
 }
 
-export default PostingPage;
+export default CommunityPage;

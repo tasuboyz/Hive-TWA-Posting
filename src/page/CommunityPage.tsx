5405b4ee60b26523bdca77c2a22ed693522ca1d9
tasuboyz <tasuhiro.davide@gmail.com>
2024-10-02 19:09:46 +0200

    Update CommunityPage Interface

--- src/page/CommunityPage.tsx
+++ src/page/CommunityPage.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback, useRef } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import './CommunityPage.css';
 import '../App.css'
 import { Telegram } from "@twa-dev/types";
@@ -20,53 +20,33 @@ function CommunityPage() {
   const [communityNames, setCommunityNames] = useState<Array<string>>([]);
   const [community, setCommunity] = useState<string>('');
   const [loading, setLoading] = useState<boolean>(false);
-  const [hasMore, setHasMore] = useState<boolean>(true);
-  const [page, setPage] = useState<number>(1);
   const navigate = useNavigate();
-  const observer = useRef<IntersectionObserver | null>(null);
 
-  const lastCommunityElementRef = useCallback((node: HTMLLIElement) => {
-  if (loading) return;
-  if (observer.current) observer.current.disconnect();
-  observer.current = new IntersectionObserver(entries => {
-    if (entries[0].isIntersecting && hasMore) {
-      setPage(prevPage => prevPage + 1);
-    }
-  });
-  if (node) observer.current.observe(node);
-}, [loading, hasMore]);
-
-  const SearchCommunity = useCallback(async (): Promise<void> => {
-    setLoading(true);
-    const headers = {
-        "accept": "application/json",
-        "Content-Type": "application/json"
-    };
-
-    try {
-        const response = await fetch(`https://imridd.eu.pythonanywhere.com/api/hive/communities?title=${community}&page=${page}&limit=20`, {
-            method: 'GET',
-            headers: headers,
-        });
-
-        if (!response.ok) {
-            throw new Error('Error fetching communities');
-        }
-        const data = await response.json(); 
-        const newCommunities = data.map((item: Community) => `${item.name},${item.title}`);
-        setCommunityNames(prev => [...prev, ...newCommunities]);
-        setHasMore(newCommunities.length > 0);
-        setLoading(false);
-    } catch (error) {
-        window.Telegram.WebApp.showPopup({
-            title: "Error",
-            message: "An error occurred while fetching communities.",
-            buttons: [{ type: 'ok' }]
-        });
-        console.error('Error fetching communities:', error);
-        setLoading(false);
-    }
-  }, [community, page]);
+const SearchCommunity = useCallback(async (): Promise<void> => {
+  setLoading(true);
+  const headers = { "accept": "application/json", "Content-Type": "application/json" };
+  try {
+      const response = await fetch(`https://imridd.eu.pythonanywhere.com/api/hive/communities?title=${community}&limit=20`, {
+          method: 'GET',
+          headers: headers,
+      });
+      if (!response.ok) {
+          throw new Error('Error fetching communities');
+      }
+      const data = await response.json();
+      const newCommunities = data.map((item: Community) => `${item.name},${item.title}`);
+      setCommunityNames(newCommunities);
+      setLoading(false);
+  } catch (error) {
+      window.Telegram.WebApp.showPopup({
+          title: "Error",
+          message: "An error occurred while fetching communities.",
+          buttons: [{ type: 'ok' }]
+      });
+      console.error('Error fetching communities:', error);
+      setLoading(false);
+  }
+}, [community]);
 
   useEffect(() => {
     window.Telegram.WebApp.BackButton.show();
@@ -75,11 +55,15 @@ function CommunityPage() {
       navigate('/Hive-TWA-Posting/post');
     });
 
-    SearchCommunity();
-  }, [navigate, SearchCommunity, page]);
+      SearchCommunity();
+  }, [SearchCommunity, navigate]);
 
   const handleCommunityChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setCommunity(event.target.value);
+    const filteredCommunities = communityNames.filter(name => 
+      name.toLowerCase().includes(event.target.value.toLowerCase())
+    );
+    setCommunityNames(filteredCommunities);
   };
 
   const handleCommunitySelect = (selectedName: string, selectedId: string) => {
@@ -113,7 +97,7 @@ function CommunityPage() {
           const [id, name] = item.split(',');
           if (communityNames.length === index + 1) {
             return (
-              <ListItem ref={lastCommunityElementRef} key={index} onClick={() => handleCommunitySelect(name, id)}>
+              <ListItem key={index} onClick={() => handleCommunitySelect(name, id)}>
                 <ListItemText primary={name} />
               </ListItem>
             );
